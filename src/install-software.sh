5ab511d28a84dad34df97c44eaf79264ae93083d
Jonathon Gosselin <jgosselin@startmail.com>
2024-09-01 11:10:30 -0400

    Add SDR++ Installation

--- src/install-software.sh
+++ src/install-software.sh
@@ -38,9 +38,20 @@ rm -f packages.microsoft.gpg
 
 # Largely based off of OBS Studio's page:
 # https://obsproject.com/download
-add-apt-repository ppa:obsproject/obs-studio
+add-apt-repository -y ppa:obsproject/obs-studio
 
 apt-get install -y apt-transport-https
 
 apt-get update
 apt-get install -y firefox code ffmpeg obs-studio
+
+# Download and install SDR++
+wget -O /tmp/sdrpp_ubuntu_noble_amd64.deb https://github.com/AlexandreRouma/SDRPlusPlus/releases/download/nightly/sdrpp_ubuntu_noble_amd64.deb
+if [ -f '/tmp/sdrpp_ubuntu_noble_amd64.deb' ]; then
+	dpkg --install /tmp/sdrpp_ubuntu_noble_amd64.deb
+	apt-get --fix-broken install -y
+else
+	echo === WARNING! ===
+	echo SDR++ did not download, skipping installation.
+	echo === WARNING! ===
+fi
