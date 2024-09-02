d2e31568cf0ed77d8d283cb0f9459855277f63f1
Jonathon Gosselin <jgosselin@startmail.com>
2024-09-02 12:14:22 -0400

    Fix installation from read-only medium

--- src/install-software.sh
+++ src/install-software.sh
@@ -31,17 +31,17 @@ fi
 
 # Largely based off of Microsoft's page:
 # https://code.visualstudio.com/docs/setup/linux
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/packages.microsoft.gpg
 
 if [ ! -f '/etc/apt/keyrings/packages.microsoft.gpg' ]; then
-	install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+	install -D -o root -g root -m 644 /tmp/packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 fi
 
 if [ ! -f '/etc/apt/sources.list.d/vscode.list' ]; then
 	echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | tee /etc/apt/sources.list.d/vscode.list > /dev/null
 fi
 
-rm -f packages.microsoft.gpg
+rm -f /tmp/packages.microsoft.gpg
 
 # Largely based off of OBS Studio's page:
 # https://obsproject.com/download
